c058f1157a001c6c7d158816ba1912985bce730f
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-03-15 22:14:23 -0700

    added if/else to js, *functions were never added*

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const x = 23, y = String("what is love")
+
+let j = 69, z = true
+
+if (j === x) {
+    result = 'they match'
+}else {
+    result = 'no luck'
+}
+
+for (const k of y) {
+    console.log(k);
+}
